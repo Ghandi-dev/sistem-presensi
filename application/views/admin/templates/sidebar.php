ca41b2a5cd4e5cb76f39ed1e281a06ea4c019381
Ghandi-dev <scoutghandi31@gmail.com>
2024-10-22 00:02:12 +0700

    fix: logo

--- application/views/admin/templates/sidebar.php
+++ application/views/admin/templates/sidebar.php
@@ -5,7 +5,7 @@
         <i class="fas fa-times p-3 cursor-pointer text-secondary opacity-5 position-absolute end-0 top-0 d-none d-xl-none"
             aria-hidden="true" id="iconSidenav"></i>
         <a class="navbar-brand m-0" href="<?php echo base_url('admin') ?>">
-            <img src="<?php echo base_url() ?>assets/img/logo-desa-cigelam.png ?>">
+            <img src="<?php echo base_url() ?>assets/img/logo-desa-cigelam.png">
             <span class="ms-1 font-weight-bold">Sistem Presensi</span>
         </a>
     </div>
